0d36ea853ac81c93ab41de98e6c32804b879c661
souravmajhi22 <46593149+souravmajhi22@users.noreply.github.com>
2019-08-03 23:51:12 +0530

    Update Problem_3.java

--- Problem_3.java
+++ Problem_3.java
@@ -3,14 +3,28 @@ public class Problem_3
 {
 	  public static void main(String[] args)
 	  {
-	        long num = 600851475143L;
-	        long i = 2;
-	        for ( ; i <= num; i++)
-	        {
-	            while (num % i == 0) {
-	                num /= i;
-	            }
-	        }
-	        System.out.println(i-1);
-	    }
+	        long n=600851475143L,c=0;
+		for(long i=n-2;i>=1;i=i-2)
+		{
+			if(n%i==0)
+			{
+			for(long j=1;j<=i;j++)
+			{
+				if(i%j==0)
+				{
+					c++;
+					if(c>2)
+					break;
+				}
+			}
+			if(c==2)
+			{
+				System.out.println(i);
+				break;
+			}
+			c=0;
+			}	
+		}
+	}
 }
+// output : 6857
