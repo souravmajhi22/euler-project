cc9136469772680e63635159e9e84e925514fa57
souravmajhi22 <46593149+souravmajhi22@users.noreply.github.com>
2019-08-04 23:54:53 +0530

    Update Problem_4.java

--- Problem_4.java
+++ Problem_4.java
@@ -1,39 +1,31 @@
 // program:Largest palindrome product
 public class Problem_4 
 {
-	public static long reverse(long n)
+	public static int reverse(int n)
 	{
-		long r=0,d;
+		int r=0,d;
 		while(n>0)
 		{
 			d=n%10;
 			r=r*10+d;
 			n=n/10;
 		}
-		return r;	
-	}
-	public static long largestPalindromeproduct(long a,long b)
-	{
-		long  i,j,p,m=0;
-		for(i=a;i>99;i--)
-		{
-			for(j=b;j>=i;j--)
-			{
-				p=i*j;
-				if(isPalindrome(p))
-				m=(long)Math.max(m, p);	
-				}
-			}
-			return m;
-	}
-	public static boolean isPalindrome(long n)
-	{
-		return reverse(n)==n;
+		return r;
 	}
 	public static void main(String args[]) 
 	{
-		long lp=largestPalindromeproduct(999,999);
-		System.out.println(lp);
-		
+		int  p;
+		int m=0;
+			for(int i=100;i<=999;i++)
+			{
+				for(int j=i;j<=999;j++)
+				{
+					p=i*j;
+					int k=reverse(p);
+					if(p==k)
+					m=Math.max(m, k);
+				}
+			}
+	System.out.println(m);
 	}
 }
