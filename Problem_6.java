81a1f28753cb329c625e6c293a5d06a3160cfc4e
souravmajhi22 <46593149+souravmajhi22@users.noreply.github.com>
2019-08-05 19:47:10 +0530

    Update Problem_6.java

--- Problem_6.java
+++ Problem_6.java
@@ -4,14 +4,8 @@ public class Problem_6
 	public static void main(String[] args)
 	{
 		int s1=0,s2=0;
-		for(int i=1;i<=100;i++)
-		{
-			s1=s1+(i*i);
-		}
-		for(int i=1;i<=100;i++)
-		{
-			s2=s2+i;
-		}
+		s2=(100*101)/2;
+	    	s1=(100*101*201)/6;
 		System.out.println("Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is "+((s2*s2)-s1));
 	}
 }
