950e7bd4d2a85ed546db7b43f7844acc9c01c040
souravmajhi22 <46593149+souravmajhi22@users.noreply.github.com>
2019-08-05 19:40:05 +0530

    Update Problem_3.java

--- Problem_3.java
+++ Problem_3.java
@@ -4,15 +4,19 @@ public class Problem_3
 	  public static void main(String[] args)
 	  {
 	        long num = 600851475143L;
-	        long i = 2;
-	        for ( ; i <= num; i++)
+	        long i = 3,max;
+	        for ( ; i <Math.sqrt(num);i=i+2)
 	        {
 	            while (num % i == 0) 
 		    {
 	                num /= i;
 	            }
 	        }
-	        System.out.println(i-1);
-	    }
+	        if(num>2)
+	      {
+	         max=num;
+	         System.out.println("largest prime factor is ="+ max);
+	      }
+	}
 }
 // output : 6857
